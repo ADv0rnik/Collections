fd814531b7aabc5c214989afa4951c80b86cf8a5
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-02-17 01:28:43 +0300

    Modified class

--- Task1/src/by/gsu/epamlab/FirstPurchaseMap.java
+++ Task1/src/by/gsu/epamlab/FirstPurchaseMap.java
@@ -5,4 +5,13 @@ import java.util.HashMap;
 public class FirstPurchaseMap {
     private HashMap<Purchase, String> firstMap;
 
+    public HashMap<Purchase, String> getFirstMap() {
+        return firstMap;
+    }
+
+    public FirstPurchaseMap() {
+        firstMap = new HashMap<Purchase, String>();
+    }
+
+
 }

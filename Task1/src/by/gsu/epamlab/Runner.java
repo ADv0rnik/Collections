587ba73bc7d271e0497305a24c581aa9fd0a26fe
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-02-21 23:16:43 +0300

    Added enumMap

--- Task1/src/by/gsu/epamlab/Runner.java
+++ Task1/src/by/gsu/epamlab/Runner.java
@@ -12,13 +12,28 @@ public class Runner {
         Scanner sc;
         HashMap<Purchase, String>firstPurchaseMap = new HashMap<>();
         HashMap<Purchase, String>lastPurchaseMap = new HashMap<>();
+        List<PricePurchase> list = new ArrayList<>();
+        EnumMap<WeekDay, List<PricePurchase>> enumMap = new EnumMap<>(WeekDay.class);
         try{
             sc = new Scanner(new FileReader(PACKAGE + args[0] + EXT));
             while (sc.hasNextLine()){
                 String key = sc.nextLine();
                 String value = sc.nextLine();
                 Purchase purchase = PurchaseFactory.getClassFromFactory(key);
-                if(firstPurchaseMap.containsKey(purchase)){
+
+                if(purchase.getClass().equals(PricePurchase.class)){
+                    list.add((PricePurchase) purchase);
+                    WeekDay day = WeekDay.valueOf(value);
+                    if (enumMap.get(day) == null){
+                        enumMap.put(day, new ArrayList<>());
+                        enumMap.get(day).add((PricePurchase) purchase);
+                    }
+                    else {
+                        enumMap.get(day).add((PricePurchase) purchase);
+                    }
+                }
+
+                if(firstPurchaseMap.containsKey(purchase)) {
                     int day1 = WeekDay.valueOf(value).ordinal();
                     int day2 = WeekDay.valueOf(firstPurchaseMap.get(purchase)).ordinal();
                     if (day1 < day2) {
@@ -26,10 +41,12 @@ public class Runner {
                         removeItem(firstPurchaseMap, purchase);
                         firstPurchaseMap.put(purchase, value);
                         lastPurchaseMap.put(purchase, tmp);
-                    }else{
+                    }
+                    else{
                         lastPurchaseMap.put(purchase,value);
                     }
-                }else {
+                }
+                else {
                     firstPurchaseMap.put(purchase, value);
                 }
             }
@@ -40,12 +57,15 @@ public class Runner {
         System.out.println();
         printMap(lastPurchaseMap);
         System.out.println();
+        printMap(enumMap);
     }
 
     private static void printMap(HashMap<Purchase, String> map) {
-        map.forEach((Purchase, String) -> {
-            System.out.format("%s = %s%n", Purchase, String);
-        });
+        map.forEach((Purchase, String) -> System.out.format("%s = %s%n", Purchase, String));
+    }
+
+    private static void printMap(EnumMap<WeekDay, List<PricePurchase>> map) {
+        map.forEach((WeekDay, list) -> System.out.format("%s = %s%n", WeekDay, list));
     }
 
     private static void removeItem (HashMap<Purchase, String> map, Purchase k){
--- Task1/src/by/gsu/epamlab/WeekDay.java
+++ Task1/src/by/gsu/epamlab/WeekDay.java
@@ -1,11 +1,11 @@
 package by.gsu.epamlab;
 
 public enum WeekDay {
-    SUNDAY,
     MONDAY,
     TUESDAY,
     WEDNESDAY,
     THURSDAY,
     FRIDAY,
-    SATURDAY;
+    SATURDAY,
+    SUNDAY;
 }
